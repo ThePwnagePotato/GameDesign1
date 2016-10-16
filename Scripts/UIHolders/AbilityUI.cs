5b400d6c2c763ed30d70750d59e70d95791d67c1
Vlad Laptop <vbarvinko@live.com>
2016-10-16 23:37:34 +0200

    new UI stuff

--- Scripts/UIHolders/AbilityUI.cs
+++ Scripts/UIHolders/AbilityUI.cs
@@ -11,6 +11,7 @@ public class AbilityUI : MonoBehaviour {
 
 	public Text nameText;
 	public Text powText;
+	public Text cooldownText;
 	public Text upScaleText;
 	public Text downScaleText;
 	public Text maxMoveText;
@@ -18,25 +19,29 @@ public class AbilityUI : MonoBehaviour {
 	public Text totalUpMoveText;
 	public Text totalDownMoveText;
 	public Text totalSideMoveText;
+	public Slider cooldownSlider;
 
 	void Start() {
 		gameManager = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
 	}
 
 	public void PushAbilitySelect () {
-		if (gameManager.gameStack.Peek().type == GameStateType.SELECTEDUNIT
+		if (gameManager.gameStack.Peek ().type == GameStateType.SELECTEDABILITY)
+			gameManager.Pop ();
+		if (gameManager.gameStack.Peek ().type == GameStateType.SELECTEDUNIT
 			&& gameManager.gameStack.Peek().evoker.GetComponent<Unit>() == unit
 			&& gameManager.gameStack.Peek().evoker.GetComponent<Unit>().isFriendly()
 			&& unit.canAttack
 			&& ability.cooldown <= 0) {
-			GameState gameState = new GameState (GameStateType.SELECTEDABILITY, ability.gameObject);
-			gameManager.Push (gameState);
+				GameState gameState = new GameState (GameStateType.SELECTEDABILITY, ability.gameObject);
+				gameManager.Push (gameState);
 		}
 	}
 
 	public void UpdateValues(Ability selectedAbility, Unit selectedUnit) {
 		ability = selectedAbility;
 		unit = selectedUnit;
+		nameText.text = selectedAbility.getName ();
 		powText.text = selectedAbility.getDamage(selectedUnit.power).ToString();
 		upScaleText.text = selectedAbility.upScale.ToString();
 		downScaleText.text = selectedAbility.downScale.ToString();
@@ -45,5 +50,8 @@ public class AbilityUI : MonoBehaviour {
 		totalUpMoveText.text = selectedAbility.upRange().ToString();
 		totalDownMoveText.text = selectedAbility.downRange().ToString();
 		totalSideMoveText.text = selectedAbility.sideRange().ToString();
+		cooldownText.text = selectedAbility.cooldown.ToString();
+		cooldownSlider.maxValue = selectedAbility.maxCooldown();
+		cooldownSlider.value = selectedAbility.cooldown;
 	}
 }

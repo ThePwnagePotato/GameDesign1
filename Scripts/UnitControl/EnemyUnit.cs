51023cffba778e12a84a9c01c8c25d9fc50237b0
PwnagePotato <krui.zhang@gmail.com>
2016-10-16 16:21:18 +0200

    Small things?

--- Scripts/UnitControl/EnemyUnit.cs
+++ Scripts/UnitControl/EnemyUnit.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public abstract class EnemyUnit : Unit {
 
 	public abstract Unit targetUnit { get; set; }
 
 	/* in a turn:
+	 * - Decide what to do (move / attack)
 	 * - Get Target unit (closest vs lowest health?)
 	 * - Move towards target unit if not in range
 	 * - bool with always higher/lower for positioning?
@@ -13,20 +15,39 @@ public abstract class EnemyUnit : Unit {
 	*/
 
 	new public void ResetTurn() {
+		//resets targetUnit before calling the base function, so that the taunt status effect works
 		targetUnit = null;
 		base.ResetTurn();
 	}
 
-	public void DoTurn() {
-		int maxRange = 0;
 
-		//find the longest range ability, and highest damage ability.
+	List<Ability> availableAbilities = new List<Ability> ();
+	Ability maxRangeAbility = null;
+	Ability highestDamageAbility = null;
+
+	public void DoTurn() {
+		availableAbilities.Clear ();
+		maxRangeAbility = null;
+		highestDamageAbility = null;
+
+		//get available abilities (cooldown <= 0)
 		foreach (GameObject abilityObject in abilities) {
 			Ability ability = abilityObject.GetComponent<Ability>();
-			if (ability.maxRange () > maxRange) {
-				maxRange = ability.maxRange ();
-			}
+			if (ability.cooldown <= 0) {
+				availableAbilities.Add (ability);
 
+				//find the longest range ability, and highest damage ability.
+				if (maxRangeAbility == null || ability.maxRange () > maxRangeAbility.maxRange ()) {
+					maxRangeAbility = ability;
+				}
+				if (highestDamageAbility == null || ability.getDamage (currentPower) > highestDamageAbility.getDamage (currentPower)) {
+					highestDamageAbility = ability;
+				}
+			}
+		}
+		//if there are no available abilities, set canAttack to false (canAttack is reset to true in ResetTurn())
+		if (availableAbilities.Count == 0) {
+			canAttack = false;
 		}
 
 		//if the enemy does not have a target yet, acquire one
@@ -42,5 +63,27 @@ public abstract class EnemyUnit : Unit {
 		foreach (Unit unit in gameManager.boardManager.friendlyUnits) {
 
 		}
+	}
+
+
+	//distance < highest damage range
+	//distance < max range
+	//distance < moves + max range
+	private bool isTargetReachable (Vector3 targetPosition) {
+
+		return false;
+	}
+
+	//first look through all targets
+	//get highest health killable
+	//get lowest health while highest damageable
+	//get lowest health damageable
+
+	//make moveto method, calls move mulitple times
+
+	private Dictionary<ReachableTile, List<Vector3>> possibleMoveList = new Dictionary<ReachableTile, List<Vector3>> ();
+
+	void MoveTo (Vector3 endTarget) {
+		
 	}
 }
